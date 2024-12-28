c95bc0c180c81d79a4b362587df5d38fddc9a499
PerssonO <1984.ola.persson@gmail.com>
2024-12-28 22:06:24 +0100

    first
    Från återskapning efter haveri, vet ej om det fungerar

---
+++ src/Tester.java
@@ -0,0 +1,12 @@
+public class Tester {
+    public static void main(String[] args) {
+        APIController c = new APIController();
+        for (int i = 0; i < 12 ; i++) {
+            AiMove move = c.newMove();
+            System.out.println(move);
+        }
+
+
+
+    }
+}
