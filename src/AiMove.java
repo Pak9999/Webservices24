36bcc375973c1cddd8282590246fc2a38a916d3d
PerssonO <1984.ola.persson@gmail.com>
2024-12-28 22:06:24 +0100

    first
    Från återskapning efter haveri, vet ej om det fungerar

---
+++ src/AiMove.java
@@ -0,0 +1,44 @@
+public class AiMove {
+   private String adress;
+   private String name;
+   private int number;
+
+    public AiMove(String adress, int number, String name) {
+        this.adress = adress;
+        this.number = number;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString() {
+        return "AiMove{" +
+                "adress='" + adress + '\'' +
+                ", name='" + name + '\'' +
+                ", number=" + number +
+                '}';
+    }
+}
---
+++ src/AzureAPI.java
@@ -0,0 +1,48 @@
+import io.github.cdimascio.dotenv.Dotenv;
+import kong.unirest.core.HttpResponse;
+import kong.unirest.core.JsonNode;
+import kong.unirest.core.Unirest;
+import kong.unirest.core.json.JSONArray;
+import kong.unirest.core.json.JSONObject;
+
+import java.security.SecureRandom;
+import java.util.HashMap;
+
+public class AzureAPI {
+    private String apiKey;
+    private String endPoint = "https://bilar.cognitiveservices.azure.com/vision/v3.2/analyze?visualFeatures=Objects";
+    private HashMap<String,String> map = new HashMap<>();
+    public AzureAPI(String apiKey){
+        this.apiKey = apiKey;
+    }
+    public int countCars(String adress) {
+        map.clear();
+        map.put("url", adress);
+        try {
+            HttpResponse<JsonNode> response = Unirest.post(endPoint)
+                    .header("Ocp-Apim-Subscription-Key", apiKey)
+                    .header("Content-Type", "application/json")
+                    .body(map)
+                    .asJson();
+
+            if (response.getStatus() == 200){
+                JSONObject j = response.getBody().getObject();
+                JSONArray a = j.getJSONArray("objects");
+                //System.out.println(a.toString());
+                return a.length();
+            }
+            else {
+                System.out.println("Nått gick fel i Azure retunerar 0");
+               return 0;
+            }
+        }
+        catch (Exception e){
+            System.out.println("Nått gick fel i Azure retunerar 0");
+            return 0;
+        }
+
+
+
+
+    }
+}
